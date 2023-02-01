f0697c24f85007e79521d4cb9bc71a05fa5d0230
joshua.renwick <joshua.renwick@boohooo.com>
2023-02-01 11:34:47 +0000

    First commit

---
+++ src/main/java/com/chessdemon/Chess/Demon/ChessDemonApplication.java
@@ -0,0 +1,13 @@
+package com.chessdemon.Chess.Demon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessDemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessDemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chessdemon/Chess/Demon/ChessDemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessdemon.Chess.Demon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessDemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
